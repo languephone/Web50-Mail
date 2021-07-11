b761ca193ca694e5ba9a530ca2870b68e78f7c5b
languephone <73309519+languephone@users.noreply.github.com>
2021-07-11 17:56:48 +0100

    Added Admin functionality.

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, Email
+
 # Register your models here.
+class EmailAdmin(admin.ModelAdmin):
+	list_display = ("sender", "subject", "read")
+
+admin.site.register(User)
+admin.site.register(Email, EmailAdmin)
